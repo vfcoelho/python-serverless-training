c41503380ad9ea8d7fd60d9d4b08d6dc09cbd527
Victor <99798304@ambev.com.br>
2019-10-16 20:16:18 -0300

    adds __init__.py files so local libraries can work

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,16 +1,16 @@
 import os
+from functions.wishlist_handler import claim_gift
 class TestBase(object):
-
+    
     def setup(self):
-
+    
         os.environ['dbname'] = "wishlist"
         os.environ['user'] = 'postgres'
         os.environ['password'] = 'postgres'
         os.environ['host'] = "localhost"
 
     def test_base(self):
-
-        from functions.wishlist_handler import claim_gift
+    
 
         # event = {'body':'{"id":1}'}
         event = {'body':'{"id":1}','requestContext':{'stage':'local'}}
